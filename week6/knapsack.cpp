596e1c37adc368cc36dd92b6f39d04d3054a92d8
LMJW <heysuperming@gmail.com>
2019-01-01 15:38:58 +1100

    knapsack

--- week6/knapsack.cpp
+++ week6/knapsack.cpp
@@ -3,23 +3,38 @@
 
 using std::vector;
 
-int optimal_weight(int W, const vector<int> &w) {
-  //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
+int optimal_weight(int W, const vector<int>& w) {
+    int n = w.size();
+    vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+    for (int i = 0; i < n + 1; ++i) {
+        for (int j = 0; j < W + 1; ++j) {
+            if (i == 0 || j == 0) {
+                ;
+            } else if (j < w[i - 1]) {
+                dp[i][j] = dp[i - 1][j];
+            } else {
+                int r = j - w[i - 1];
+                int t1 = dp[i - 1][r] + w[i - 1];
+                int t2 = dp[i - 1][j];
+                dp[i][j] = std::max(t1, t2);
+            }
+        }
     }
-  }
-  return current_weight;
+    // for (auto& row : dp) {
+    //     std::cout << "\n";
+    //     for (auto col : row) {
+    //         std::cout << col << " ";
+    //     }
+    // }
+    return dp[n][W];
 }
 
 int main() {
-  int n, W;
-  std::cin >> W >> n;
-  vector<int> w(n);
-  for (int i = 0; i < n; i++) {
-    std::cin >> w[i];
-  }
-  std::cout << optimal_weight(W, w) << '\n';
+    int n, W;
+    std::cin >> W >> n;
+    vector<int> w(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> w[i];
+    }
+    std::cout << optimal_weight(W, w) << '\n';
 }
