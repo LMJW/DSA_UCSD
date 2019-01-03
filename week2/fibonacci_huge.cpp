1d071818520f0b02ead880c917eaa772b1a1d2dd
LMJW <heysuperming@gmail.com>
2019-01-03 23:08:03 +1100

    add c++ huge

--- week2/fibonacci_huge.cpp
+++ week2/fibonacci_huge.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
+#include <vector>
+
+using std::vector;
 
 long long get_fibonacci_huge_naive(long long n, long long m) {
-    if (n <= 1)
-        return n;
-
-    long long previous = 0;
-    long long current  = 1;
-
-    for (long long i = 0; i < n - 1; ++i) {
-        long long tmp_previous = previous;
-        previous = current;
-        current = tmp_previous + current;
+    vector<long long> table;
+    table.push_back(0);
+    table.push_back(1);
+    for (long long i = 2;; ++i) {
+        long long t = table[i - 1] + table[i - 2];
+        t %= m;
+        table.push_back(t);
+        if (t == 1 && table[i - 1] == 0) {
+            break;
+        }
     }
+    int l = table.size() - 1;
+    int r = n % l;
 
-    return current % m;
+    for (auto e : table) {
+        std::cout << e << " ";
+    }
+    std::cout << " \n";
+
+    return table[r];
 }
 
 int main() {
