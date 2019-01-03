2e830aeb448c118b2a6e7b14a17303399bc6dfe9
LMJW <heysuperming@gmail.com>
2019-01-04 10:51:36 +1100

    week 2 q7

--- week2/fibonacci_partial_sum.cpp
+++ week2/fibonacci_partial_sum.cpp
@@ -3,22 +3,30 @@
 using std::vector;
 
 long long get_fibonacci_partial_sum_naive(long long from, long long to) {
-    long long sum = 0;
-
-    long long current = 0;
-    long long next  = 1;
-
-    for (long long i = 0; i <= to; ++i) {
-        if (i >= from) {
-            sum += current;
+    vector<long long> fb = {0, 1};
+    vector<long long> sum = {0, 1};
+    for (int i = 2;; ++i) {
+        int t = fb[i - 1] + fb[i - 2];
+        t %= 10;
+        fb.push_back(t);
+        if (t == 1 && fb[i - 1] == 0) {
+            break;
         }
-
-        long long new_current = next;
-        next = next + current;
-        current = new_current;
     }
+    int l = fb.size() - 2;
 
-    return sum % 10;
+    for (int i = 2;; ++i) {
+        int t = sum[i - 1] + fb[i % l];
+        t %= 10;
+        sum.push_back(t);
+        if (t == 1 && sum[i - 1] == 0) {
+            break;
+        }
+    }
+    int ll = sum.size() - 2;
+
+    int res = sum[to % ll] + 10 - sum[from % ll] + fb[from % l];
+    return res % 10;
 }
 
 int main() {
