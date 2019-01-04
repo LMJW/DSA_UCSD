320314fd10e87646f564b3aa3211118e30b67a89
LMJW <heysuperming@gmail.com>
2019-01-04 11:11:40 +1100

    week2 q8

--- week2/fibonacci_sum_squares.cpp
+++ week2/fibonacci_sum_squares.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
+#include <vector>
+
+using std::vector;
 
 int fibonacci_sum_squares_naive(long long n) {
     if (n <= 1)
         return n;
-
-    long long previous = 0;
-    long long current  = 1;
-    long long sum      = 1;
-
-    for (long long i = 0; i < n - 1; ++i) {
-        long long tmp_previous = previous;
-        previous = current;
-        current = tmp_previous + current;
-        sum += current * current;
+    vector<int> fb = {0, 1};
+    for (int i = 2;; i++) {
+        int t = fb[i - 1] + fb[i - 2];
+        t %= 10;
+        fb.push_back(t);
+        if (t == 1 && fb[i - 1] == 0) {
+            break;
+        }
     }
+    int l = fb.size() - 2;
 
-    return sum % 10;
+    return fb[(n + 1) % l] * fb[n % l] % 10;
 }
 
 int main() {
