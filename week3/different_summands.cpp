95fa35c6dd77ed826dbe6f966b99218134485bd8
LMJW <heysuperming@gmail.com>
2019-01-04 22:13:18 +1100

    week3 greedy q5

--- week3/different_summands.cpp
+++ week3/different_summands.cpp
@@ -4,17 +4,25 @@
 using std::vector;
 
 vector<int> optimal_summands(int n) {
-  vector<int> summands;
-  //write your code here
-  return summands;
+    vector<int> summands;
+    for (int i = 1;; ++i) {
+        if (n > 2 * i) {
+            summands.push_back(i);
+            n -= i;
+        } else {
+            summands.push_back(n);
+            break;
+        }
+    }
+    return summands;
 }
 
 int main() {
-  int n;
-  std::cin >> n;
-  vector<int> summands = optimal_summands(n);
-  std::cout << summands.size() << '\n';
-  for (size_t i = 0; i < summands.size(); ++i) {
-    std::cout << summands[i] << ' ';
-  }
+    int n;
+    std::cin >> n;
+    vector<int> summands = optimal_summands(n);
+    std::cout << summands.size() << '\n';
+    for (size_t i = 0; i < summands.size(); ++i) {
+        std::cout << summands[i] << ' ';
+    }
 }
