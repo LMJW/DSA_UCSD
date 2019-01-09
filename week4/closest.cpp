2ea7d51ebfcce32a55eb86eeef653b7e21e696d6
LMJW <heysuperming@gmail.com>
2019-01-09 21:55:57 +1100

    merge sort to sort points base on x coordinate

--- week4/closest.cpp
+++ week4/closest.cpp
@@ -11,8 +11,47 @@ using std::pair;
 using std::string;
 using std::vector;
 
+vector<pair<int, int>> mergesort(vector<pair<int, int>> ps) {
+    if (ps.size() < 2) {
+        return ps;
+    }
+    vector<pair<int, int>> res;
+    vector<pair<int, int>> left(ps.begin(), ps.begin() + ps.size() / 2);
+    vector<pair<int, int>> right(ps.begin() + ps.size() / 2, ps.end());
+    left = mergesort(left);
+    right = mergesort(right);
+    size_t i, j, c;
+    i = 0;
+    j = 0;
+    for (c = 0; c < ps.size(); ++c) {
+        if (i < left.size() && j < right.size()) {
+            if (left[i].first < right[j].first) {
+                res.push_back(left[i]);
+                ++i;
+            } else {
+                res.push_back(right[j]);
+                ++j;
+            }
+        } else if (i < left.size()) {
+            res.push_back(left[i]);
+            ++i;
+        } else {
+            res.push_back(right[j]);
+            ++j;
+        }
+    }
+    return res;
+}
+
 double minimal_distance(vector<int> x, vector<int> y) {
-    // write your code here
+    vector<pair<int, int>> points;
+    for (size_t i = 0; i < x.size(); ++i) {
+        points.push_back(std::make_pair(x[i], y[i]));
+    }
+    points = mergesort(points);
+    for (auto e : points) {
+        std::cout << e.first << " " << e.second << " \n";
+    }
     return 0.;
 }
 
