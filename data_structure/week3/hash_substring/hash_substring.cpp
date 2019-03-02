707dea91312a46eb5dd4fbde801861cd4108210a
LMJW <heysuperming@gmail.com>
2019-03-03 08:50:22 +1100

    Improve the hash function efficiency by reduce math computation

--- data_structure/week3/hash_substring/hash_substring.cpp
+++ data_structure/week3/hash_substring/hash_substring.cpp
@@ -21,11 +21,11 @@ Data read_input() {
     return data;
 }
 
-size_t hash_func(string s) {
+// only calculate at the first time
+size_t hash_func(const string& s, int start, int pattern) {
     size_t res = 0;
-    for (int i = s.size() - 1; i >= 0; --i)
+    for (int i = start + pattern - 1; i >= start; --i)
         res = (res * MULTIPLIER + s[i]) % PRIMAR;
-
     return res;
 }
 
@@ -40,26 +40,37 @@ std::vector<int> get_occurrences(const Data& input) {
     vector<int> ans;
     // define the parameter for my own hash function
 
-    int ts = static_cast<int>(t.size());
-    int ss = static_cast<int>(s.size());
+    long long _POWER_MOD = 1;
+    for (int i = 0; i < s.size(); ++i)
+        _POWER_MOD = (_POWER_MOD * MULTIPLIER) % PRIMAR;
 
     list<size_t> hashvals;
-    for (int i = ts - ss; i > -1; --i) {
+    for (int i = t.size() - s.size(); i > -1; --i) {
         if (hashvals.size() == 0) {
-            hashvals.push_back(hash_func(t.substr(i, i + ss - 1)));
+            hashvals.push_back(hash_func(t, i, s.size()));
         } else {
             long long lv = hashvals.front();
             long long t1 = (lv * MULTIPLIER) % PRIMAR;
             long long t2 = t[i];
-            long long t3 = t[i + ss];
-            for (int j = 0; j < ss; ++j)
-                t3 = (t3 * MULTIPLIER) % PRIMAR;
+            // long long t3 = t[i + s.size()];
+            /// This part can be computational intensive as we need to go
+            /// through this process for all substrings
+            /// (a*x^m)%p= a%p * x^m%p
+            /// so we can calculate x^m first and then do the multiplication
+
+            /// the following commented code's time complexity is a bit too high
+            /**
+             * for (int j = 0; j < s.size(); ++j)
+             *   t3 = (t3 * MULTIPLIER) % PRIMAR;
+             */
+            long long t3 = t[i + s.size()] * _POWER_MOD % PRIMAR;
+
             lv = (t1 + t2 - t3 + PRIMAR) % PRIMAR;
 
             hashvals.push_front(lv);
         }
     }
-    auto ph = hash_func(s);
+    auto ph = hash_func(s, 0, s.size());
     int idx = 0;
     for (auto iter = hashvals.begin(); iter != hashvals.end(); ++iter) {
         if (*iter == ph) {
