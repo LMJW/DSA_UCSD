fb4b8a9a831870d05a9be040c090b28691756f31
LMJW <heysuperming@gmail.com>
2019-03-09 17:43:37 +1100

    Week 6 assignment tree traverse

---
+++ data_structure/week4/tree_orders/tree-orders.cpp
@@ -0,0 +1,150 @@
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <vector>
+
+#if defined(__unix__) || defined(__APPLE__)
+#include <sys/resource.h>
+#endif
+
+using std::cin;
+using std::cout;
+using std::function;
+using std::ios_base;
+using std::vector;
+
+class TreeOrders {
+    int n;
+    vector<int> key;
+    vector<int> left;
+    vector<int> right;
+
+public:
+    void read() {
+        std::ifstream ifs(
+            "/Users/will/github.com/DSA_UCSD/data_structure/week4/tree_orders/"
+            "tests/21");
+
+        cin.rdbuf(ifs.rdbuf());
+
+        cin >> n;
+        key.resize(n);
+        left.resize(n);
+        right.resize(n);
+        for (int i = 0; i < n; i++) {
+            cin >> key[i] >> left[i] >> right[i];
+        }
+    }
+
+    vector<int> in_order() {
+        vector<int> result;
+
+        _inorder(result, key, left, right, 0);
+
+        return result;
+    }
+
+    vector<int> pre_order() {
+        vector<int> result;
+        std::function<void(int)> f;
+
+        f = [&](int idx) {
+            result.push_back(key[idx]);
+            if (left[idx] != -1) {
+                f(left[idx]);
+            }
+            if (right[idx] != -1) {
+                f(right[idx]);
+            }
+        };
+        if (n > 0) {
+            f(0);
+        }
+        return result;
+    }
+
+    vector<int> post_order() {
+        vector<int> result;
+        std::function<void(int)> f;
+        f = [&](int idx) {
+            auto k = key[idx];
+            auto l = left[idx];
+            auto r = right[idx];
+            if (l != -1) {
+                f(l);
+            }
+            if (r != -1) {
+                f(r);
+            }
+
+            result.push_back(k);
+        };
+        if (n > 0) {
+            f(0);
+        }
+        return result;
+    }
+
+    void _inorder(vector<int>& res,
+                  const vector<int>& key,
+                  const vector<int>& left,
+                  const vector<int>& right,
+                  int idx) {
+        int l = left[idx];
+        int k = key[idx];
+        int r = right[idx];
+
+        if (l != -1) {
+            _inorder(res, key, left, right, l);
+        }
+        res.push_back(k);
+        if (r != -1) {
+            _inorder(res, key, left, right, r);
+        }
+        return;
+    }
+};
+
+void print(vector<int> a) {
+    for (size_t i = 0; i < a.size(); i++) {
+        if (i > 0) {
+            cout << ' ';
+        }
+        cout << a[i];
+    }
+    cout << '\n';
+}
+
+int main_with_large_stack_space() {
+    ios_base::sync_with_stdio(0);
+    TreeOrders t;
+    t.read();
+    print(t.in_order());
+    // print(t.pre_order());
+    // print(t.post_order());
+    return 0;
+}
+
+int main(int argc, char** argv) {
+#if defined(__unix__) || defined(__APPLE__)
+    // Allow larger stack space
+    const rlim_t kStackSize = 16 * 1024 * 1024;  // min stack size = 16 MB
+    struct rlimit rl;
+    int result;
+
+    result = getrlimit(RLIMIT_STACK, &rl);
+    if (result == 0) {
+        if (rl.rlim_cur < kStackSize) {
+            rl.rlim_cur = kStackSize;
+            result = setrlimit(RLIMIT_STACK, &rl);
+            if (result != 0) {
+                std::cerr << "setrlimit returned result = " << result
+                          << std::endl;
+            }
+        }
+    }
+#endif
+
+    return main_with_large_stack_space();
+}
